01c15f6329a1d8ba10d9491db0bf6fa807d9637b
ElliosAins <valerashow12@gmail.com>
2025-04-26 12:32:09 +0300

    host

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host:'0.0.0.0',
     port: process.env.PORT || 5173,
-    allowedHosts: ['all'],
+    allowedHosts: ['rendertry-a1x3.onrender.com','all'],
   }
 })
